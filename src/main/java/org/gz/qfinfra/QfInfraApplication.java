ce9bb2fc03b8698ab2e944d503b12460c1890bd8
guozhong <17853522814@163.com>
2025-11-15 13:10:06 +0800

    init

---
+++ src/main/java/org/gz/qfinfra/QfInfraApplication.java
@@ -0,0 +1,13 @@
+package org.gz.qfinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfInfraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QfInfraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gz/qfinfra/QfInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gz.qfinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QfInfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
